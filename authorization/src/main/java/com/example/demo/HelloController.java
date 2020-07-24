4e265997baa4d8f15d37101ad4abc6065bd8f46a
Avik1998Sarkar <36466256+Avik1998Sarkar@users.noreply.github.com>
2020-07-24 18:23:21 +0530

    Add files via upload

---
+++ authorization/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String sayHello() {
+		return "<h1>Hello ........... </h1>";
+	}
+	
+
+	@GetMapping("/user")
+	public String sayHelloToUser() {
+		return "<h1>Hello User ........... </h1>";
+	}
+	
+	
+
+	@GetMapping("/admin")
+	public String sayHelloToAdmin() {
+		return "<h1>Hello Admin........... </h1>";
+	}
+
+}
---
+++ spring-boot-security-h2/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into users values('user','pass',true);
+insert into users values('admin','pass',true);
+insert into users values('avik','sar1998kar',true);
+
+insert into authorities values('user','ROLE_USER');
+insert into authorities values('admin','ROLE_ADMIN');
+insert into authorities values('avik','ROLE_AVIK');
